46afc37c325c634e388a07c772d9673474ec14be
Michel Falke <michel@yiggle.nl>
2020-06-22 14:46:07 +0200

    feat(delivery + availability): eerste opzet
    - Sync de order en match met cached versie, zodat je status updates kunt krijgen over je order
    - Sla beschikbaarheden op in globale tags, zodat je hiermee ook beschikbaarheid kunt opvragen van een  specifieke dag

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ class JumboSupermarket extends Homey.App {
 
         this.tokens = {};
 
-        // this.deliveryStatus = new Homey.FlowCardTrigger('delivery_status');
-        // this.orderStatus().then(this.log('bezorg status gesynced')).catch(this.error);
-        // setInterval(this.orderStatus.bind(this), 60 * 1000 * 15);
-        //
-        // this.availabilityTokens().then(this.log('beschikbaarheid gesynced')).catch(this.error);
-        // setInterval(this.availabilityTokens.bind(this), 60 * 1000 * 5);
-        this.addProductToCart('211761ZK');
+        this.deliveryStatus = new Homey.FlowCardTrigger('delivery_status').register();
+        this.orderStatus().then(this.log('bezorg status gesynced')).catch(this.error);
+        setInterval(this.orderStatus.bind(this), 60 * 1000 * 15);
+
+        this.availabilityTokens().then(this.log('beschikbaarheid gesynced')).catch(this.error);
+        setInterval(this.availabilityTokens.bind(this), 60 * 1000 * 5);
+        // this.addProductToCart('211761ZK');
     }
 
     async addProductToCart() {
@@ -42,7 +42,6 @@ class JumboSupermarket extends Homey.App {
                 const deliveryOrPickupDate = DateHelper.getDutchDay(order.orderAvailableDate.getDay()) + ' ' + order.orderAvailableDate.getDate() + ' ' + DateHelper.getDutchMonth(order.orderAvailableDate.getMonth());
 
                 this.deliveryStatus
-                    .register()
                     .trigger({
                         delivery_status: deliveryOrPickupDate + ' tussen ' + order.readableTime
                     })
@@ -57,17 +56,12 @@ class JumboSupermarket extends Homey.App {
     }
 
     async availabilityTokens() {
-        const thursday = DateHelper.getNextDay('thursday', true);
-        const friday = DateHelper.getNextDay('friday', true);
-        const saturday = DateHelper.getNextDay('saturday', true);
-
-        let tokens = {
-            'delivery_next_thursday': [],
-            'delivery_next_friday': [],
-            'delivery_next_saturday': [],
-        };
-
+        const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
         let tokenIndex = 'undefined';
+        let tokens = [];
+        Object.keys(this.tokens).forEach(key => {
+            Homey.ManagerFlow.unregisterToken(this.tokens[key]).catch(this.error);
+        })
 
         return await this.jumbo.deliveryOptions().then(result => {
             if (result.timeSlots === undefined) {
@@ -75,91 +69,48 @@ class JumboSupermarket extends Homey.App {
             }
             result.timeSlots.data.forEach(item => {
                 let dateObj = new Date(item.day);
-                if (dateObj.getTime() === thursday.getTime()) {
-                    tokenIndex = 'delivery_next_thursday';
-                } else if (dateObj.getTime() === friday.getTime()) {
-                    tokenIndex = 'delivery_next_friday';
-                } else if (dateObj.getTime() === saturday.getTime()) {
-                    tokenIndex = 'delivery_next_saturday';
-                } else {
-                    return;
-                }
-
-                item.timeSlots.forEach(timeslot => {
-                    if (timeslot.available === false) {
+                days.forEach(day => {
+                    let nextDay = DateHelper.getNextDay(day, true);
+                    if (dateObj.getTime() !== nextDay.getTime()) {
                         return;
                     }
 
-                    let startTime = new Date(timeslot.startDateTime);
-                    let formattedStartTime = startTime.getHours() + ':' + ("0" + startTime.getMinutes()).substr(-2);
-                    let endTime = new Date(timeslot.endDateTime);
-                    let formattedEndTime = endTime.getHours() + ':' + ("0" + endTime.getMinutes()).substr(-2);
-                    tokens[tokenIndex].push(formattedStartTime + ' tot ' + formattedEndTime);
-                })
-            });
+                    tokenIndex = 'delivery_next_' + day;
+                    item.timeSlots.forEach(timeslot => {
+                        if (timeslot.available === false) {
+                            return;
+                        }
 
-            tokens.delivery_next_thursday = tokens.delivery_next_thursday.join(' van ').toString();
-            if (tokens.delivery_next_thursday === '') {
-                tokens.delivery_next_thursday = 'Geen mogelijkheden komende donderdag';
-            } else {
-                tokens.delivery_next_thursday = 'Mogelijke bezorgtijden komende donderdag ' + tokens.delivery_next_thursday;
-            }
+                        let startTime = new Date(timeslot.startDateTime);
+                        let formattedStartTime = startTime.getHours() + ':' + ("0" + startTime.getMinutes()).substr(-2);
+                        let endTime = new Date(timeslot.endDateTime);
+                        let formattedEndTime = endTime.getHours() + ':' + ("0" + endTime.getMinutes()).substr(-2);
 
-            tokens.delivery_next_friday = tokens.delivery_next_friday.join(' van ').toString();
-            if (tokens.delivery_next_friday === '') {
-                tokens.delivery_next_friday = 'Geen mogelijkheden komende vrijdag';
-            } else {
-                tokens.delivery_next_friday = 'Mogelijke bezorgtijden komende vrijdag ' + tokens.delivery_next_friday;
-            }
+                        if (tokens[tokenIndex] === undefined) {
+                            tokens[tokenIndex] = [];
+                        }
+                        tokens[tokenIndex].push(formattedStartTime + ' tot ' + formattedEndTime);
+                    })
 
-            tokens.delivery_next_saturday = tokens.delivery_next_saturday.join(' van ').toString();
-            if (tokens.delivery_next_saturday === '') {
-                tokens.delivery_next_saturday = 'Geen mogelijkheden komende zaterdag';
-            } else {
-                tokens.delivery_next_saturday = 'Mogelijke bezorgtijden komende zaterdag ' + tokens.delivery_next_saturday;
-            }
+                    let concattedTokenValue = Homey.ManagerSettings.get('no-timeslot').toString();
+                    if (tokens[tokenIndex] !== undefined && tokens[tokenIndex].length > 0) {
+                        concattedTokenValue = tokens[tokenIndex].join(' , ');
+                    }
 
-            Object.keys(this.tokens).forEach(key => {
-                Homey.ManagerFlow.unregisterToken(this.tokens[key]).catch(this.error);
-            })
-
-            this.tokens['thursday'] = new Homey.FlowToken( 'delivery_next_thursday', {
-                type: 'string',
-                title: 'Bezorgtijden komende donderdag',
-                example: tokens.delivery_next_thursday
-            });
-            this.tokens['thursday'].register()
-                .then(() => {
-                    return this.tokens['thursday'].setValue( tokens.delivery_next_thursday);
-                })
-                .catch( err => {
-                    this.error( err );
+                    this.tokens[day] = new Homey.FlowToken('delivery_next_' + day, {
+                        type: 'string',
+                        title: 'Bezorgtijden komende ' + DateHelper.getDutchDay(nextDay.getDay()),
+                        example: concattedTokenValue
+                    });
+                    this.tokens[day].register()
+                        .then(() => {
+                            return this.tokens[day].setValue(concattedTokenValue);
+                        })
+                        .catch(err => {
+                            this.error(err);
+                        });
                 });
-
-            this.tokens['friday'] = new Homey.FlowToken( 'delivery_next_friday', {
-                type: 'string',
-                title: 'Bezorgtijden komende vrijdag'
             });
-            this.tokens['friday'].register()
-                .then(() => {
-                    return this.tokens['friday'].setValue( tokens.delivery_next_friday);
-                })
-                .catch( err => {
-                    this.error( err );
-                });
-
-            this.tokens['saturday'] = new Homey.FlowToken( 'delivery_next_saturday', {
-                type: 'string',
-                title: 'Bezorgtijden komende zaterdag',
-                example: tokens.delivery_next_saturday
-            });
-            this.tokens['saturday'].register()
-                .then(() => {
-                    return this.tokens['saturday'].setValue( tokens.delivery_next_saturday);
-                })
-                .catch( err => {
-                    this.error( err );
-                });
         });
     }
 }
